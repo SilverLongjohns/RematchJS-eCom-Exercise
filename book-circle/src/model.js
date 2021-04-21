8f88109c323e43fede854c671cfe44e4b881a494
JakeCummings <Jake.Cummings@rtgs.com>
2021-04-21 17:55:39 +0100

    added total tallying in basket

--- book-circle/src/model.js
+++ book-circle/src/model.js
@@ -1,15 +1,27 @@
 export default {
 
     state: {
-        cart: []
+        cart: [],
+        total: 0,
     },
     reducers: {
         addToBasketSuccess(state, data) {
-            var { cart } = state
+            var { cart } = state;
             cart.push(data);
             return {
                 ...state,
                 cart: cart,
+            }
+        },
+        updateTotalSuccess(state) {
+            var { cart } = state;
+            var total = 0;
+            for (let book of cart) {
+                total += book.price;
+            }
+            return {
+                ...state,
+                total: total,
             }
         }
     },
@@ -17,6 +29,14 @@ export default {
         addToBasket(book) {
             try {
                 this.addToBasketSuccess(book);
+                this.updateTotal();
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        updateTotal() {
+            try {
+                this.updateTotalSuccess();
             } catch (err) {
                 console.log(err)
             }
--- book-circle/src/model.test.js
+++ book-circle/src/model.test.js
@@ -6,7 +6,7 @@ describe("A book", () => {
     beforeEach(() => {
     store = init({
         models: {
-            books: { ...model, state: { cart: [] } },
+            books: { ...model, state: { cart: [], total: 0 } },
         },
     })
     })
@@ -16,4 +16,20 @@ describe("A book", () => {
         const { books } = getState();
         expect(books.cart).toEqual(["book1"])
     });
+
+    it("should be able to add multiple books to the basket", () => {
+        const{dispatch, getState} = store;
+        dispatch.books.addToBasket("book1")
+        dispatch.books.addToBasket("book2")
+        dispatch.books.addToBasket("book3")
+        const {books} = getState();
+        expect(books.cart).toEqual(["book1", "book2", "book3"])
+    })
+
+    it("should update price when book is added to basket", () => {
+        const{dispatch, getState} = store;
+        dispatch.books.addToBasket({title: "book1", price: 10});
+        const {books} = getState();
+        expect(books.total).toEqual(10);
+    })
 });
